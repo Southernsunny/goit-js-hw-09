066b5adce5c2ffad4767513337092f869711a031
Olia Medvetska <olia.medvetska1967@gmail.com>
2023-04-09 22:23:44 +0300

    add hw-9

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,29 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  intervalId = setInterval(changeBodyBgColor, 1000);
+}
+
+function onStopBtnClick() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(intervalId);
+}
+
+function changeBodyBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
